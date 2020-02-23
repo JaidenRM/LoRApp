b9abb23a8f939c98665ece53a46b1ceb83b38161
Jaiden Muscat <jaidenreecemuscat@gmail.com>
2020-02-23 21:04:42 +1100

    menu list and transitions for fade in and out

--- react-app/src/Hamburger.js
+++ react-app/src/Hamburger.js
@@ -1,15 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Hamburger.css'
+import NavBar from './NavBar';
 
 class Hamburger extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false,
+            isFirstTime: true
+        };
+
+        //The binding enables `this` to work in the callback
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(state => ({
+            isOpen: !state.isOpen,
+            isFirstTime: false
+        }));
+    }
+
+    setAnimations(cName) {
+        cName += "-line";
+        if(this.state.isFirstTime)
+            return cName;
+
+        cName += (" " + cName + "-animate");
+
+        if(!this.state.isOpen)
+            cName += "-reverse";
+
+        return cName;
+    }
+
     render() {
         return(
-            <div>
-                <div className="top-line"></div>
-                <div className="mid-line"></div>
-                <div className="bot-line"></div>
+            <div onClick={this.handleClick}>
+                <NavBar className={this.state.isOpen ? "show" : "hidden"}/>
+                <div className={this.setAnimations("top")}/>
+                <div className={this.setAnimations("mid")}/>
+                <div className={this.setAnimations("bot")}/>
             </div>
         );
     }
--- react-app/src/NavBar.js
+++ react-app/src/NavBar.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './NavBar.css';
 
 class NavBar extends React.Component {
 
+    constructor(props) {
+        super(props)
+    }
+
     render() {
-        return <h2>I am a NavBar</h2>;
+        return (
+            <ul className={this.props.className}>
+                <li>Home</li>
+                <li>Top Players</li>
+                <li>Deck Builder</li>
+                <li>Card Gallery</li>
+                <li>Decks Submitted</li>
+                <li>Card Statistics</li>
+                <li>About</li>
+            </ul>
+        );
     }
 }
 
