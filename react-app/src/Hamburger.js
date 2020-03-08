ee9e5433a49e3834c59a37a159f37005d0b05c4f
Jaiden Muscat <jaidenreecemuscat@gmail.com>
2020-03-08 22:27:54 +1100

    adding some basic html for each page fixing up menu order

--- react-app/src/Hamburger.js
+++ react-app/src/Hamburger.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './Hamburger.css'
 import NavBar from './NavBar';
 
--- react-app/src/card-statistics.js
+++ react-app/src/card-statistics.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
 class CardStatistics extends React.Component {
     render() {
         return (
